a9195a205d08e95f6738f927b170bf5651b67a0f
Naman Saini <namansaini464@gmail.com>
2026-03-29 16:45:42 +0530

    changes

---
+++ src/Pages/onlineJournal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SerialContributionForm from '../Components/SerialContribution/SerialContribution';
+import CitContainer from '../Components/Container';
+
+const OnlineJournal = () => {
+  return (
+    <CitContainer title="Online Journal">
+      <SerialContributionForm />
+    </CitContainer>
+  );
+}
+
+export default OnlineJournal;
